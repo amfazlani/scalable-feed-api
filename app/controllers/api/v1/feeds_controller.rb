a15fd64d531f85cc4c7d0cb1502b1c4d4e673223
Amaar Fazlani <fazlaniamaar@gmail.com>
2026-05-12 03:48:32 -0500

    Move feeds controller to api v1 namespace and update routes

---
+++ app/controllers/api/v1/feeds_controller.rb
@@ -0,0 +1,18 @@
+module Api
+  module V1
+    class FeedsController < ApplicationController
+      # before_action :authenticate_user!
+
+      def index
+        feed_items = FeedItem
+          .where(user_id: User.first.id)
+          .includes(post: [:user])
+          .order(created_at: :desc)
+          .page(params[:page])
+          .per(10)
+
+        render json: FeedSerializer.new(feed_items).as_json
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
-      get "/feed", to: "feed#index"
+      get "/feed", to: "feeds#index"
 
       resources :posts, only: [:create, :show]
       resources :subscriptions, only: [:create, :destroy]
